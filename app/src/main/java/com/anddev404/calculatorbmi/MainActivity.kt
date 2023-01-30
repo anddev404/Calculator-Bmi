fd2379d94b8e65f4079fb132e14d7952d5dc69c3
anddev404 <marcinek07@gmail.com>
2023-01-30 22:32:35 +0100

    create instance of SharedViewModel in MainActivity

--- app/src/main/java/com/anddev404/calculatorbmi/MainActivity.kt
+++ app/src/main/java/com/anddev404/calculatorbmi/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.anddev404.calculatorbmi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewmodel: SharedViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewmodel = ViewModelProvider(this)[SharedViewModel::class.java]
+
     }
 }
