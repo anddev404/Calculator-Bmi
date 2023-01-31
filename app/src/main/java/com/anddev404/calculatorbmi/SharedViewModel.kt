0a2ba50019d5c1d0914f69c93e4f2d01131a14e4
anddev404 <marcinek07@gmail.com>
2023-01-31 22:16:21 +0100

    add bmi to SharedViewModel

--- app/src/main/java/com/anddev404/calculatorbmi/SharedViewModel.kt
+++ app/src/main/java/com/anddev404/calculatorbmi/SharedViewModel.kt
@@ -1,19 +1,35 @@
 package com.anddev404.calculatorbmi
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.anddev404.calculatorbmi.tools.CalculatorTools
 
 class SharedViewModel : ViewModel() {
 
-    private val _height: MutableLiveData<Float> = MutableLiveData()
+    private val _height: MutableLiveData<Float> = MutableLiveData(180f)//TODO delete temporary value
     val height: LiveData<Float> = _height
 
-    private val _weight: MutableLiveData<Float> = MutableLiveData()
+    private val _weight: MutableLiveData<Float> = MutableLiveData(80f)//TODO delete temporary value
     val weight: LiveData<Float> = _weight
 
-    init {
+    val bmi = MediatorLiveData<Float>()
 
+    init {
+        bmi.apply {
+
+            addSource(_height) { height ->
+                _weight.value?.let { weight ->
+                    value = CalculatorTools.calculateBmi(height, weight)
+                }
+            }
+            addSource(_weight) { weight ->
+                _height.value?.let { height ->
+                    value = CalculatorTools.calculateBmi(height, weight)
+                }
+            }
+        }
     }
 
     //region methods
