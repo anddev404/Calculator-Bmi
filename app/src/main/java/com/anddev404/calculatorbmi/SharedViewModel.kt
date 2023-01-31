db267eca85c612bc2f49e7b3c0c256fb4b57259a
anddev404 <marcinek07@gmail.com>
2023-01-31 18:31:20 +0100

    add height to SharedViewModel

--- app/src/main/java/com/anddev404/calculatorbmi/SharedViewModel.kt
+++ app/src/main/java/com/anddev404/calculatorbmi/SharedViewModel.kt
@@ -1,6 +1,20 @@
 package com.anddev404.calculatorbmi
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class SharedViewModel : ViewModel() {
+
+    private val _height: MutableLiveData<Float> = MutableLiveData()
+    val height: LiveData<Float> = _height
+
+    //region methods
+    fun changeHeight(height: Float) {
+        if (_height.value != height) {
+            _height.value = height
+        }
+    }
+    //endregion
+
 }
