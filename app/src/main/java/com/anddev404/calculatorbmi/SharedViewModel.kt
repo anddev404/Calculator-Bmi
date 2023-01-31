dcbe5e33fd331ca7c7ea09059c27d2678216deab
anddev404 <marcinek07@gmail.com>
2023-01-31 18:32:20 +0100

    add weight to SharedViewModel

--- app/src/main/java/com/anddev404/calculatorbmi/SharedViewModel.kt
+++ app/src/main/java/com/anddev404/calculatorbmi/SharedViewModel.kt
@@ -9,10 +9,18 @@ class SharedViewModel : ViewModel() {
     private val _height: MutableLiveData<Float> = MutableLiveData()
     val height: LiveData<Float> = _height
 
+    private val _weight: MutableLiveData<Float> = MutableLiveData()
+    val weight: LiveData<Float> = _weight
     //region methods
     fun changeHeight(height: Float) {
         if (_height.value != height) {
             _height.value = height
+        }
+    }
+
+    fun changeWeight(weight: Float) {
+        if (_weight.value != weight) {
+            _weight.value = weight
         }
     }
     //endregion
