0c844fa65bcc6e0772c7171530b7f791697a9f17
anddev404 <marcinek07@gmail.com>
2023-02-05 13:37:32 +0100

    add setOnClickListener to changeWeightUnitButton

--- app/src/main/java/com/anddev404/calculatorbmi/MainActivity.kt
+++ app/src/main/java/com/anddev404/calculatorbmi/MainActivity.kt
@@ -2,8 +2,10 @@ package com.anddev404.calculatorbmi
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import com.anddev404.calculatorbmi.data.model.WeightUnit
 import com.anddev404.calculatorbmi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -23,10 +25,34 @@ class MainActivity : AppCompatActivity() {
             }
 
         hideActionBar()
-
+        addMenuButtonsClickListener()
     }
 
     private fun hideActionBar() {
         supportActionBar?.hide()
+    }
+
+    private fun addMenuButtonsClickListener() {
+
+        binding.changeWeightUnitButton.setOnClickListener {
+            val popup = PopupMenu(this, it)
+            popup.menuInflater.inflate(R.menu.weight_popup_menu, popup.menu)
+
+            popup.setOnMenuItemClickListener { menuItem ->
+                when (menuItem.title) {
+                    resources.getString(R.string.kg) -> {
+                        viewmodel.changeWeightUnit(WeightUnit.KG)
+                    }
+                    resources.getString(R.string.lb) -> {
+                        viewmodel.changeWeightUnit(WeightUnit.LB)
+                    }
+                    resources.getString(R.string.st_lb) -> {
+                        viewmodel.changeWeightUnit(WeightUnit.ST_LB)
+                    }
+                }
+                true
+            }
+            popup.show()
+        }
     }
 }
