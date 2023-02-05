6bdc1ec18c5cd73ed93f767d60648142caafc2cf
anddev404 <marcinek07@gmail.com>
2023-02-05 13:35:03 +0100

    Update MainActivity.kt

--- app/src/main/java/com/anddev404/calculatorbmi/MainActivity.kt
+++ app/src/main/java/com/anddev404/calculatorbmi/MainActivity.kt
@@ -9,18 +9,18 @@ import com.anddev404.calculatorbmi.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewmodel: SharedViewModel
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         viewmodel = ViewModelProvider(this)[SharedViewModel::class.java]
 
-        val binding: ActivityMainBinding =
-            DataBindingUtil.setContentView<ActivityMainBinding?>(this, R.layout.activity_main)
-                .apply {
-                    viewmodel = this@MainActivity.viewmodel
-                    lifecycleOwner = this@MainActivity
-                }
+        binding = DataBindingUtil.setContentView<ActivityMainBinding?>(this, R.layout.activity_main)
+            .apply {
+                viewmodel = this@MainActivity.viewmodel
+                lifecycleOwner = this@MainActivity
+            }
 
         hideActionBar()
 
