12d8fdd9cdcf56d78a550eedb815c79b993518bc
anddev404 <marcinek07@gmail.com>
2023-02-06 11:47:50 +0100

    Create UnitConverterTest.kt

--- app/src/test/java/com/anddev404/calculatorbmi/UnitConverterTest.kt
+++ app/src/test/java/com/anddev404/calculatorbmi/UnitConverterTest.kt
@@ -61,4 +61,62 @@ class UnitConverterTest {
         assertEquals("3 ft 3 in", UnitConverter.convertHeight(100f, HeightUnit.FT_IN))
 
     }
+
+    @Test
+    fun lbToKg() {
+
+        assertEquals(0f, UnitConverter.lbToKg(0), 0.1f)
+        assertEquals(0.4f, UnitConverter.lbToKg(1), 0.1f)
+        assertEquals(4.5f, UnitConverter.lbToKg(10), 0.1f)
+        assertEquals(45.3f, UnitConverter.lbToKg(100), 0.1f)
+    }
+
+    @Test
+    fun stoneToKg() {
+
+        assertEquals(0f, UnitConverter.stoneToKg(0), 0.1f)
+        assertEquals(6.3f, UnitConverter.stoneToKg(1), 0.1f)
+        assertEquals(63.5f, UnitConverter.stoneToKg(10), 0.1f)
+        assertEquals(635.0f, UnitConverter.stoneToKg(100), 0.1f)
+    }
+
+    @Test
+    fun stoneAndLbToKg() {
+
+        assertEquals(0f, UnitConverter.stoneAndLbToKg(0, 0), 0.1f)
+        assertEquals(6.3f, UnitConverter.stoneAndLbToKg(1, 0), 0.1f)
+        assertEquals(0.4f, UnitConverter.stoneAndLbToKg(0, 1), 0.1f)
+        assertEquals(6.8f, UnitConverter.stoneAndLbToKg(1, 1), 0.1f)
+        assertEquals(68.0f, UnitConverter.stoneAndLbToKg(10, 10), 0.1f)
+        assertEquals(680.3f, UnitConverter.stoneAndLbToKg(100, 100), 0.1f)
+    }
+
+    @Test
+    fun inchesToCm() {
+
+        assertEquals(0f, UnitConverter.inchesToCm(0), 0.1f)
+        assertEquals(2.54f, UnitConverter.inchesToCm(1), 0.1f)
+        assertEquals(25.4f, UnitConverter.inchesToCm(10), 0.1f)
+        assertEquals(254.0f, UnitConverter.inchesToCm(100), 0.1f)
+    }
+
+    @Test
+    fun feetToCm() {
+
+        assertEquals(0f, UnitConverter.feetToCm(0), 0.1f)
+        assertEquals(30.5f, UnitConverter.feetToCm(1), 0.1f)
+        assertEquals(304.8f, UnitConverter.feetToCm(10), 0.1f)
+        assertEquals(3048f, UnitConverter.feetToCm(100), 0.1f)
+    }
+
+    @Test
+    fun feetAndInchesToCm() {
+
+        assertEquals(0f, UnitConverter.feetAndInchesToCm(0, 0), 0.1f)
+        assertEquals(30.5f, UnitConverter.feetAndInchesToCm(1, 0), 0.1f)
+        assertEquals(2.5f, UnitConverter.feetAndInchesToCm(0, 1), 0.1f)
+        assertEquals(33.0f, UnitConverter.feetAndInchesToCm(1, 1), 0.1f)
+        assertEquals(330.2f, UnitConverter.feetAndInchesToCm(10, 10), 0.1f)
+        assertEquals(3302.0f, UnitConverter.feetAndInchesToCm(100, 100), 0.1f)
+    }
 }
