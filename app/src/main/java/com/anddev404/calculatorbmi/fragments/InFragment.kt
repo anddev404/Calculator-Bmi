f7db33eee8e073b80961f47943361d13b577a718
anddev404 <marcinek07@gmail.com>
2023-02-16 21:21:33 +0100

    Update InFragment.kt

--- app/src/main/java/com/anddev404/calculatorbmi/fragments/InFragment.kt
+++ app/src/main/java/com/anddev404/calculatorbmi/fragments/InFragment.kt
@@ -1,13 +1,22 @@
 package com.anddev404.calculatorbmi.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.anddev404.calculatorbmi.R
+import androidx.lifecycle.ViewModelProvider
+import com.anddev404.calculatorbmi.SharedViewModel
+import com.anddev404.calculatorbmi.databinding.FragmentInBinding
+import com.anddev404.calculatorbmi.extensions.toIntOrZero
+import com.anddev404.calculatorbmi.tools.UnitConverter
 
 class InFragment : Fragment() {
+
+    private lateinit var viewmodel: SharedViewModel
+    private lateinit var binding: FragmentInBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +25,23 @@ class InFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_in, container, false)
+    ): View {
+
+        viewmodel = ViewModelProvider(requireActivity())[SharedViewModel::class.java]
+        binding = FragmentInBinding.inflate(inflater, container, false).apply {
+            viewmodel = this@InFragment.viewmodel
+            lifecycleOwner = this@InFragment
+        }
+        binding.inEditText.addTextChangedListener(object : TextWatcher {
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {
+                viewmodel.changeHeight(UnitConverter.inchesToCm(p0.toString().toIntOrZero()))
+
+            }
+        })
+
+        return binding.root
     }
 }
