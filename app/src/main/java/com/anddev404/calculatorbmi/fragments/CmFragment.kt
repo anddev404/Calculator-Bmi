a3da0ae70e6fbd9bda113740dd6f3cd769b0a955
anddev404 <marcinek07@gmail.com>
2023-02-16 21:21:36 +0100

    Update CmFragment.kt

--- app/src/main/java/com/anddev404/calculatorbmi/fragments/CmFragment.kt
+++ app/src/main/java/com/anddev404/calculatorbmi/fragments/CmFragment.kt
@@ -1,13 +1,21 @@
 package com.anddev404.calculatorbmi.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.anddev404.calculatorbmi.R
+import androidx.lifecycle.ViewModelProvider
+import com.anddev404.calculatorbmi.SharedViewModel
+import com.anddev404.calculatorbmi.databinding.FragmentCmBinding
+import com.anddev404.calculatorbmi.extensions.toFloatOrZero
 
 class CmFragment : Fragment() {
+
+    private lateinit var viewmodel: SharedViewModel
+    private lateinit var binding: FragmentCmBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +24,22 @@ class CmFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cm, container, false)
+    ): View {
+
+        viewmodel = ViewModelProvider(requireActivity())[SharedViewModel::class.java]
+        binding = FragmentCmBinding.inflate(inflater, container, false).apply {
+            viewmodel = this@CmFragment.viewmodel
+            lifecycleOwner = this@CmFragment
+        }
+        binding.cmEditText.addTextChangedListener(object : TextWatcher {
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {
+                viewmodel.changeHeight(p0.toString().toFloatOrZero())
+            }
+        })
+
+        return binding.root
     }
 }
